01eb1df5e30def381194403c4dce872260ddd4cb
jj7pndtn2n-lang <jj7pndtn2n@privaterelay.appleid.com>
2026-08-18 22:00:15 +0700

    Create build.gradle.kts

---
+++ app/src/main/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.skintools"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.skintools"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 3
+        versionName = "3.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity:1.10.1")
+}
