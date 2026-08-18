65500a9a64ee869945db44972918ced55ac3ba4f
jj7pndtn2n-lang <jj7pndtn2n@privaterelay.appleid.com>
2026-08-18 22:23:05 +0700

    Create supabase-config.js

---
+++ app/src/main/assets/supabase-config.js
@@ -0,0 +1,6 @@
+window.SUPABASE_CONFIG = {
+  url: "https://bnzhspkwkhdymdektmzh.supabase.co",
+  anonKey: "sb_publishable_B-qTboqeIFCOs5au73gSbw_O-9RzZTo",
+  getKeyUrl: "https://link4m.org/5RuGUbD8",
+  destinationUrl: "https://kiemtienonlinee.vercel.app/taiapp.html"
+};
